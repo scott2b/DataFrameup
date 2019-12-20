a02975f4207337310e030cd96466010913dc89b8
Scott Bradley <scottbradley.mail@gmail.com>
2019-12-19 19:35:52 -0600

    ajax response in flask example

--- examples/flask/app.py
+++ examples/flask/app.py
@@ -1,12 +1,16 @@
+import os
 from flask import Flask, jsonify, render_template, request, url_for
 import pandas as pd
 import frameup
 
 app = Flask(__name__)
 
-df = pd.read_csv('/Users/scott/Downloads/ks-projects-201801.csv')
+df = pd.read_csv(os.environ['CSV_FILE'])
 
 @app.route('/mydataframe')
 def main():
     data = df.frameup.data(path=url_for('main'), **request.args)
-    return render_template('example.j2.html', **data)
+    if 'text/html' in request.accept_mimetypes:
+        return render_template('example.j2.html', **data)
+    else:
+        return jsonify(**data)
--- examples/flask/run.sh
+++ examples/flask/run.sh
@@ -1,2 +1,3 @@
 export FLASK_APP=app.py
+export CSV_FILE=path-to-csv-file
 flask run
